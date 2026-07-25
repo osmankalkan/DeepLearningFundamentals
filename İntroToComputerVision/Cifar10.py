6014ae87c1dde6fac7c527f01e062bf53e2c8823
Osman Kalkan <okalkan@posta.mu.edu.tr>
2026-07-25 22:51:58 +0300

    intro to computer vision

--- İntroToComputerVision/Cifar10.py
+++ İntroToComputerVision/Cifar10.py
@@ -1,3 +1,7 @@
+import ssl
+import certifi
+ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
+
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
@@ -5,3 +9,63 @@ import torchvision
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+train_data = datasets.CIFAR10(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+test_data = datasets.CIFAR10(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+"""
+# image normalization  (yorum satırına alındı)
+class_names = train_data.classes
+
+image, label = train_data[1]
+image = image.permute(1, 2, 0)
+plt.figure(figsize=(1.2, 1.2))
+plt.imshow(image)
+plt.title(class_names[label])
+
+from torchvision import transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
+                          std=[0.2470, 0.2435, 0.2616])
+])
+
+train_data = datasets.CIFAR10(
+    root="data",
+    train=True,
+    download=True,
+    transform=transform,
+    target_transform=None
+)
+
+test_data = datasets.CIFAR10(
+    root="data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+from torch.utils.data import DataLoader
+
+BATCH_SIZE = 32
+train_dataloader = DataLoader(train_data,
+                               batch_size=BATCH_SIZE,
+                               shuffle=True)
+test_dataloader = DataLoader(test_data,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+len(train_dataloader), len(test_dataloader)
+"""
